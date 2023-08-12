30b064911c2522e4734e96bd24d423af91839c3a
pugnitator <pugnitator2000@gmail.com>
2023-08-12 21:03:57 +0300

    U1-1-2. Edit task1: add a new variable

---
+++ src/main/kotlin/lesson1/task2.kt
@@ -0,0 +1,15 @@
+package lesson1
+
+fun main () {
+    val orderQuantity: Int = 4
+    val thanksAfterOrder: String = "Спасибо за заказ!"
+    var numberOfEmployees: Int = 2000
+
+    println(orderQuantity)
+    println(thanksAfterOrder)
+//    println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+
+}
