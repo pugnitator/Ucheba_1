28d984389c71c7fd4035bc4afb8f7b14c12c9a96
pugnitator <pugnitator2000@gmail.com>
2023-08-13 21:29:34 +0300

    U1-1-3. Gagarin's takeoff and landing

---
+++ src/main/kotlin/lesson1/task3.kt
@@ -0,0 +1,24 @@
+package lesson1
+
+fun main() {
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
+// на случай, если предполагался такой вариант решения:
+//    println(year)
+//    println(hour)
+//    println(minute)
+
+    println("Взлёт:\nГод:$year\nЧас:$hour\nМинуты:$minute\n")
+
+//ниже меняем время взлета на время посадки
+    hour = "10"
+    minute = "55"
+
+// на случай, если предполагался такой вариант решения:
+//    print(hour + ":")
+//    print(minute)
+
+    println("Время посадки: $hour:$minute")
+
+}
