48e1468d73505bd152586ef1845b10664afc4fa4
pugnitator <pugnitator2000@gmail.com>
2023-08-13 20:40:26 +0300

    U1-1-4. Fix branch and variable's type

--- src/main/kotlin/lesson1/task4.kt
+++ src/main/kotlin/lesson1/task4.kt
@@ -1,7 +1,7 @@
 package lesson1
 
 fun main() {
-    val length: String = "40_868_600_000"
+    val length: Long = 40_868_600_000
     val age: Byte = 27
     val partOfDay: Float = 0.075f
     val seconds: Short = 6480
