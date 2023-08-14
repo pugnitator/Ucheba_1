a542bfc1d7a01026b2f7ad368fa4b007e86f9d7f
pugnitator <pugnitator2000@gmail.com>
2023-08-14 22:12:03 +0300

    U1-2-2. Colculation of salary and expenses

---
+++ src/main/kotlin/lesson2/task2.kt
@@ -0,0 +1,20 @@
+package lesson2
+
+fun main() {
+    val numberOfPermanentStaff = 50
+    val numberOfInterns = 30
+    val numberOfAllEmployee = numberOfPermanentStaff + numberOfInterns
+    val employeeSalary = 30000
+    val internSalary = 20000
+    val monPermanentStaffSalaryExpenses = numberOfPermanentStaff * employeeSalary
+    val monInternsSalaryExpenses = numberOfInterns * internSalary
+    val monSalaryExpenses = monPermanentStaffSalaryExpenses + monInternsSalaryExpenses
+    val averageSalary = monSalaryExpenses/numberOfAllEmployee
+
+    println("""
+        Расходы на выплату зарплаты постоянных сотрудников: $monPermanentStaffSalaryExpenses.
+        Общие расходы по ЗП после прихода стажеров: $monSalaryExpenses.
+        Средняя ЗП одного сотрудника после устройства стажеров: $averageSalary.""".trimIndent())
+
+
+}
