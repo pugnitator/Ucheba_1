994709208658cb9ddc19f47df91ea87bab5e99af
pugnitator <pugnitator2000@gmail.com>
2023-08-14 20:16:33 +0300

    U1-2-1. test

--- src/main/kotlin/lesson2/task1.kt
+++ src/main/kotlin/lesson2/task1.kt
@@ -1,5 +1,12 @@
 package lesson2
 
 fun main() {
+    val studentQuantity = 4
+    val englishGradesList = floatArrayOf(3.0f,4.0f,3.0f,5.0f)
+
+    val averageScore = englishGradesList.sum()/studentQuantity
+
+    println(String.format("%.2f", averageScore))
+    println(averageScore)
 
 }
