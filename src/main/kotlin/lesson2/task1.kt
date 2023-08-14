bcc867fc19ea490d175f197f5f37ddc59eff1aad
pugnitator <pugnitator2000@gmail.com>
2023-08-14 21:33:20 +0300

    U1-2-1. Calculation of the average score

--- src/main/kotlin/lesson2/task1.kt
+++ src/main/kotlin/lesson2/task1.kt
@@ -3,10 +3,7 @@ package lesson2
 fun main() {
     val studentQuantity = 4
     val englishGradesList = floatArrayOf(3.0f,4.0f,3.0f,5.0f)
+    val englishAverageScore = englishGradesList.sum()/studentQuantity
 
-    val averageScore = englishGradesList.sum()/studentQuantity
-
-    println(String.format("%.2f", averageScore))
-    println(averageScore)
-
+    println(String.format("%.2f", englishAverageScore))
 }
