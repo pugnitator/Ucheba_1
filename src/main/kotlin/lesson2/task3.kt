6a2b1e78c399517a4ae067bcf14201fbb1e7b86c
pugnitator <pugnitator2000@gmail.com>
2023-08-15 21:22:48 +0300

    U1-2-3. Local commit

---
+++ src/main/kotlin/lesson2/task3.kt
@@ -0,0 +1,16 @@
+package lesson2
+
+fun main() {
+    val departureHour = 9
+    val departureMinutes = 39
+    val travelTimeInMinutes = 457
+
+    val travelTimeInHours = travelTimeInMinutes / 60
+    val remainingTravelTimeInMinutes = travelTimeInMinutes % 60
+
+    val arrivalMinutes = (departureMinutes + travelTimeInMinutes) % 60
+    val arrivalHour = ((departureHour + travelTimeInHours) + (departureMinutes + travelTimeInMinutes) / 60) % 24
+
+    println(String.format("Время прибытия поезда: %02d:%02d", arrivalHour, arrivalMinutes))
+
+}
