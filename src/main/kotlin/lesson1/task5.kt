b706f94bbf9f3a6a1e84f82fa403bc32047af9c4
pugnitator <pugnitator2000@gmail.com>
2023-08-15 20:34:48 +0300

    U1-1-5. Fix padStart() to String.format

--- src/main/kotlin/lesson1/task5.kt
+++ src/main/kotlin/lesson1/task5.kt
@@ -5,18 +5,18 @@ fun main() {
 
 //узнаём, сколько целых часов
     val hours = seconds / 3600
-    val hoursWithZeros = hours.toString().padStart(2, '0')
+    val hoursWithZeros = String.format("%02d", hours)
 
 // узнаем, какой остаток секунд после расчёта часов для расчёта минут
     val secondsRemains1 = seconds % 3600
 
 // по остатку считаем, сколько целых минут
     val minutes = secondsRemains1 / 60
-    val minutesWithZeros = minutes.toString().padStart(2, '0')
+    val minutesWithZeros = String.format("%02d", minutes)
 
 // по остатку считаем остаток секунд
     val seconds1 = secondsRemains1 % 60
-    val secondsWithZeros = seconds1.toString().padStart(2, '0')
+    val secondsWithZeros = String.format("%02d", seconds1)
 
     println ("Время, проведённое Гагариным в космосе: $hoursWithZeros:$minutesWithZeros:$secondsWithZeros.")
 
