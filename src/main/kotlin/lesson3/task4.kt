a4f5836d21e4226df233ff4fdc202d7c26f3ca13
pugnitator <pugnitator2000@gmail.com>
2023-08-19 00:03:43 +0300

    U1-3-4. Recording chess moves

---
+++ src/main/kotlin/lesson3/task4.kt
@@ -0,0 +1,19 @@
+package lesson3
+
+fun main() {
+
+    var startCell = "E2"
+    var endCell = "E4"
+    var strokeNumber = "1"
+
+    println("$startCell-$endCell;$strokeNumber")
+
+//    тут походил соперник, видимо. Ход снова перешёл к тому, кто осуществлял первый ход
+
+    startCell = "D2"
+    endCell = "D3"
+    strokeNumber = "3"
+
+    println("$startCell-$endCell;$strokeNumber")
+
+}
