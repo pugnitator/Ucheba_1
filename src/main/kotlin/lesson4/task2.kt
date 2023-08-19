9d1dc6a0e5021f1fb1696eee921176e6c1906e83
pugnitator <pugnitator2000@gmail.com>
2023-08-19 19:20:23 +0300

    U1-4-2. Checking cargo parameters

--- src/main/kotlin/lesson4/task2.kt
+++ src/main/kotlin/lesson4/task2.kt
@@ -2,9 +2,16 @@ package lesson4
 
 fun main() {
 
-    val cargoWeight =
+    print("Введите вес груза в кг: ")
+    val cargoWeight = readln().toInt()
 
+    print("Введите объем груза в литрах: ")
+    val cargoVolume = readln().toInt()
 
+    println(
+        "Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории \"Average\": " +
+                "${averageMinWeight <= cargoWeight && cargoWeight <= averageMaxWeight && cargoVolume < averageVolume}"
+    )
 }
 
 const val averageMinWeight = 35
