eec3feba69eb3e39c409d8ff1a9aba66782fd81e
pugnitator <pugnitator2000@gmail.com>
2023-08-21 20:59:15 +0300

    U1-4-4. Fix code style

--- src/main/kotlin/lesson4/task4.kt
+++ src/main/kotlin/lesson4/task4.kt
@@ -4,6 +4,7 @@ fun main() {
 
 //    считаем ориентируясь на чётность дня тренировки, по чётным - руки и пресс, по нечётным - ноги и спина
 //    при входе в приложение мы получаем день тренировки подряд (если не подряд, нам присылают 1)
+
     print("Какой по счёту день тренировки: ")
     val trainingDay = readln().toInt()
     val handsExercises = true
@@ -13,17 +14,11 @@ fun main() {
 
     val evenNumberOfTrainingDay = (trainingDay % 2) == 0
 
-    val recommendation = if (evenNumberOfTrainingDay) """
-        Упражнения для рук: ${!handsExercises}
-        Упражнения для ног: ${!legsExercises}
-        Упражнения для спины: ${!backExercises}
-        Упражнения для пресса: ${!pressExercises}
-    """.trimIndent()
-    else """
-        Упражнения для рук: ${handsExercises}
-        Упражнения для ног: ${legsExercises}
-        Упражнения для спины: ${backExercises}
-        Упражнения для пресса: ${pressExercises}
+    val recommendation = """
+        Упражнения для рук: ${if (evenNumberOfTrainingDay) !handsExercises else handsExercises}
+        Упражнения для ног: ${if (evenNumberOfTrainingDay) !legsExercises else legsExercises}
+        Упражнения для спины: ${if (evenNumberOfTrainingDay) !backExercises else backExercises}
+        Упражнения для пресса: ${if (evenNumberOfTrainingDay) !pressExercises else pressExercises}
     """.trimIndent()
 
     println(recommendation)
