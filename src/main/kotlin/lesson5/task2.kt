938f05b834b3cf6514d368299314bffdcb5bd928
pugnitator <pugnitator2000@gmail.com>
2023-08-25 13:05:58 +0300

    U1-5-2. Fix code style

--- src/main/kotlin/lesson5/task2.kt
+++ src/main/kotlin/lesson5/task2.kt
@@ -6,19 +6,15 @@ fun main() {
     val yearOfBirth = readln().toIntOrNull()
     val thisYear = 2023
 
-    if (yearOfBirth == null) {
-        println("Вы ввели неверное значение. Начните сначала.")
-        return
-    }
+    if (yearOfBirth == null) println("Вы ввели неверное значение. Начните сначала.")
 
-    val userAge = (thisYear - yearOfBirth!!)
+    val userAge = (thisYear - (yearOfBirth ?: return))
 
     if (userAge >= AGE_OF_MAJORITY) println("Показать экран со скрытым контентом.")
-    else if (userAge in MIN_AGE_FOR_LIMITED_CONTENT..MAX_AGE_FOR_LIMITED_CONTENT)
+    else if (userAge in MIN_AGE_FOR_LIMITED_CONTENT until AGE_OF_MAJORITY)
         println("Показать экран с ограниченным контентом.")
     else println("Доступ к скрытому контенту запрещён.")
 }
 
 const val AGE_OF_MAJORITY = 18
 const val MIN_AGE_FOR_LIMITED_CONTENT = 16
-const val MAX_AGE_FOR_LIMITED_CONTENT = 17
