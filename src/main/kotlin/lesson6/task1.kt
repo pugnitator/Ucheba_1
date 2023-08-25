f2b3a1dcdbf2f73a275a8c81b74b18c0b5c650ad
pugnitator <pugnitator2000@gmail.com>
2023-08-25 22:55:49 +0300

    U1-6-1. Auth: local fix

--- src/main/kotlin/lesson6/task1.kt
+++ src/main/kotlin/lesson6/task1.kt
@@ -7,25 +7,25 @@ fun main() {
     print("Придумайье пароль: ")
     val password = readln().toString()
 
-    println("\nПройдите авторизацию.")
+    println("\nПройдите авторизацию для логина $login.")
     var attemptCounter = 0
 
     do {
         if (attemptCounter in 1 until NUMBER_OF_ATTEMPTS) println(
-            "\nЛогин и/или пароль были введены неверно, попробуйте ещё раз. " +
+            "\nПароль был введен неверно, попробуйте ещё раз. " +
                     "Попыток остлось: ${NUMBER_OF_ATTEMPTS - attemptCounter}"
         )
         if (attemptCounter == 3) {
-            println("\nПопыток для ввода авторизационных данных не осталось, " +
+            println("\nПопыток для ввода пароля не осталось, " +
                     "восстановите пароль или пройдите регистрацию заново.")
             return
         }
-        print("Введите логин: ")
-        val theEnteredLogin = readln().toString()
         print("Введите пароль: ")
         val theEnteredPassword = readln().toString()
         attemptCounter += 1
-    } while ((theEnteredLogin != login) || (theEnteredPassword != password))
+    } while (theEnteredPassword != password)
+
+    println("Авторизация прошла успешно!")
 
 }
 
