ae86b2e29d8798b6a99cc02145826d1157159563
pugnitator <pugnitator2000@gmail.com>
2023-08-29 22:25:59 +0300

    U1-7-5. Fix infinite loop

--- src/main/kotlin/lesson7/task5.kt
+++ src/main/kotlin/lesson7/task5.kt
@@ -2,14 +2,14 @@ package lesson7
 
 fun main() {
 
-    print("Сколько символов должно быть в пароле: ")
+    print("Сколько символов должно быть в пароле (минимальное значение - 3): ")
     val numberOfPasswordChar = readln().toIntOrNull()
     val numbers = '0'..'9'
     val lowercaseLetters = 'a'..'z'
     val capitalLetters = 'A'..'Z'
     var password = ""
 
-    if (numberOfPasswordChar == null) {
+    if (numberOfPasswordChar == null || numberOfPasswordChar < 3) {
         println("Введено некорретное значение, попробуйте заново.")
         return
     } else {
