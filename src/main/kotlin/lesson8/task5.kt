008d6428550f3158992c7233ae412f1b5daee959
pugnitator <pugnitator2000@gmail.com>
2023-09-11 12:00:37 +0300

    U1-8-5. local commit

--- src/main/kotlin/lesson8/task5.kt
+++ src/main/kotlin/lesson8/task5.kt
@@ -2,21 +2,16 @@ package lesson8
 
 fun main() {
 
-    var ingredientsForTheDish = arrayOf("0")
-
     print("Введите кол-во ингредиентов в блюде: ")
     val countOfIngredients = readln().toIntOrNull()
     if (countOfIngredients == null) {
         println("Введено неверное значение. Попробуйте заново.")
         return
     }
-
-    for (i in 0 until countOfIngredients) {
-        print("Введите название ингредиента ${i + 1}: ")
-        val newIngredient = readln()
-        if (i == 0) ingredientsForTheDish[i] = newIngredient
-        else ingredientsForTheDish += newIngredient
-    }
+//    for (i in 0 until countOfIngredients) {
+    print("Введите названия ингредиентов: ")
+    val ingredientsForTheDish = Array(countOfIngredients) { readln() }
+    println(ingredientsForTheDish.size)
 
     println("\nСоздан рецепт из $countOfIngredients ингредиентов: ")
     for (i in ingredientsForTheDish) println(i)
