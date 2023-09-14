e92a20441a9ccc6edde0f8125753e81d04dbd4b2
pugnitator <pugnitator2000@gmail.com>
2023-09-14 22:40:41 +0300

    U1-9-5. Code refactoring

--- src/main/kotlin/lesson9/task5.kt
+++ src/main/kotlin/lesson9/task5.kt
@@ -3,16 +3,11 @@ package lesson9
 fun main() {
 
     val listOfIngredients = sortedSetOf<String>()
-    var countOfIngredients = 1
     val numberOfIngredients = 5
 
     while (listOfIngredients.size < numberOfIngredients) {
-        println("Введите название ингредиента $countOfIngredients: ")
-        val ingredient = readln().toString().lowercase()
-        if (!listOfIngredients.contains<Any>(ingredient)) {
-            listOfIngredients.add(ingredient)
-            countOfIngredients += 1
-        } else println("Вы ввели ингредиент, который уже содержится в списке, он не будет записан.")
+        println("Введите название ингредиента ${listOfIngredients.size + 1}: ")
+        listOfIngredients.add(readln())
     }
 
     println("${listOfIngredients.joinToString().replaceFirstChar { it.uppercaseChar() }}.")
