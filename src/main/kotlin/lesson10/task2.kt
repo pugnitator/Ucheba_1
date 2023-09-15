d7dde1b680e7bf78658c8851465f8af48bf5c7d3
pugnitator <pugnitator2000@gmail.com>
2023-09-15 10:27:32 +0300

    U1-10-2. Add const

--- src/main/kotlin/lesson10/task2.kt
+++ src/main/kotlin/lesson10/task2.kt
@@ -7,10 +7,13 @@ fun main() {
     println("Введите пароль: ")
     val password = readln()
 
-    checkTheLengthOfTheString(login,password)
-
+    checkTheLengthOfTheString(login, password)
 }
+
 fun checkTheLengthOfTheString(inputLogin: String, inputPassword: String) {
-    if ((inputLogin.length < 4) && (inputPassword.length < 4)) println("Логин или пароль недостаточно длинные.")
+    if ((inputLogin.length < MIN_LOG_AND_PASSWORD_LENGTH) || (inputPassword.length < MIN_LOG_AND_PASSWORD_LENGTH))
+        println("Логин или пароль недостаточно длинные.")
     else println("Добро пожаловать!")
 }
+
+const val MIN_LOG_AND_PASSWORD_LENGTH = 4
