67e1dcab42e7ecc98232a7a93093ad038089354b
pugnitator <pugnitator2000@gmail.com>
2023-09-21 18:51:16 +0300

    U1-11-3. Chat rooms

--- src/main/kotlin/lesson11/task3.kt
+++ src/main/kotlin/lesson11/task3.kt
@@ -1,22 +1,44 @@
 package lesson11
 
-import java.util.Objects
-
 
 fun main() {
-    val roomUser1 = RoomUser("поцЫк123", "Фото пёсика", "пользователь заглушен")
+    val roomUser1 = RoomUser("поцЫк123", "Фото пёсика", "заглушен")
+    val roomUser2 = RoomUser("devo4ka7", "Фото котика", "разговаривает")
+    val chatRoomUsers = mutableListOf<RoomUser>(roomUser1, roomUser2)
 
-    val chatRoom1 = ChatRoom("Новости подъезда", "Красивое фото райна", roomUser1)
+    val allChatRooms: MutableSet<ChatRoom> = mutableSetOf()
+    val chatRoom1 = ChatRoom("Новости подъезда", "Красивое фото района", chatRoomUsers)
+    allChatRooms.add(chatRoom1)
+    val chatRoom2 = ChatRoom("Кино и домино", "Фото кино и домино", chatRoomUsers)
+    allChatRooms.add(chatRoom2)
+
+    println("Заходим в приложение, выводится список комнат:")
+    allChatRooms.forEach {it.showChatRoomInfo()}
+    println()
+
+    println("Перешли в комнату ${chatRoom1.name}")
+    chatRoom1.showUsers(chatRoomUsers)
+    println()
+
+    println("Долго жмём на один из аватаров: ")
+    roomUser1.showNickname()
+    roomUser1.showStatus()
+
 }
 
 class ChatRoom(
     val name: String,
     val cover: String,
-    val userList: MutableList<RoomUser>
+    val userList: MutableList<RoomUser>,
 ) {
 
-    fun showNickname () {
-        println()
+    fun showChatRoomInfo(){
+        println("$name, $cover, ${userList.size}")
+    }
+
+    fun showUsers(userList: MutableList<RoomUser>) {
+        println("Список участников чата $name:")
+        userList.forEach { it.showPhoto() }
     }
 }
 
@@ -24,4 +46,17 @@ class RoomUser(
     val nickname: String,
     val photo: String,
     val status: String,
-)
+) {
+    fun showPhoto() {
+        println(photo)
+    }
+
+    fun showNickname() {
+        println("Пользователь $nickname.")
+    }
+
+    fun showStatus() {
+        println("Статус: $status.")
+    }
+
+}
