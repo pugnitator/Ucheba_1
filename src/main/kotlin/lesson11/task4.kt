560049e2f9ab069dce871659299e01b727bd1e21
pugnitator <pugnitator2000@gmail.com>
2023-09-27 14:16:27 +0300

    U1-11-4. Delete class ContactPage. Rename methods of class Contact New class CloseContact

--- src/main/kotlin/lesson11/task4.kt
+++ src/main/kotlin/lesson11/task4.kt
@@ -1,42 +1,13 @@
 package lesson11
 
 fun main() {
-    val user1ClosePeople = mutableListOf<String>()
-    val user0 = Contact(
-        "жена",
-        null,
-        null,
-        "8(999)999-42-42",
-        "8(999)888-42-42",
-        "жена",
-        "mail.mail.ru",
-        null
-    )
-    user1ClosePeople.add(user0.firstName)
-    val user2 = Contact(
-        "подруга",
-        null,
-        null,
-        "8(999)999-42-42",
-        "8(999)888-42-42",
-        "жена",
-        "mail.mail.ru",
-        null
-    )
-    user1ClosePeople.add(user2.firstName)
-    val user3 = Contact(
-        "подруга",
-        null,
-        null,
-        "8(999)999-42-42",
-        "8(999)888-42-42",
-        "жена",
-        "mail.mail.ru",
-        null
-    )
-    user1ClosePeople.add(user3.firstName)
 
-    val user1 = Contact(
+    val closeContact1 = CloseContact(1, "жена")
+    val closeContact2 = CloseContact(2, "подруга")
+    val closeContact3 = CloseContact(3, "подруга")
+    val closeContactList = mutableSetOf(closeContact1, closeContact2, closeContact3)
+
+    val user4 = Contact(
         "First Name",
         "Second Name",
         null,
@@ -44,24 +15,18 @@ fun main() {
         "8(999)888-42-42",
         "фото мышки",
         "mail.mail.ru",
-        user1ClosePeople
+        closeContactList,
     )
-    val user1ContactPage = ContactPage(user1)
-    user1ContactPage.showContactPage(user1)
+
+    user4.getContactInfo()
+    user4.getCloseContact()
+
 }
 
-class SquareIcon(
-    val icon: String,
-    val iconName: String,
+class CloseContact(
+    val id: Int,
+    val name: String,
 )
-
-class RoundIcon(
-    val icon: String,
-) {
-    fun showIcon(): String {
-        return icon
-    }
-}
 
 class Contact(
     val firstName: String,
@@ -71,80 +36,44 @@ class Contact(
     val homeNumber: String? = null,
     var photo: String = "стандарный кружочек",
     val iCloud: String,
-    val closePeopleSet: MutableList<String>? = null,
+    val closePeopleSet: MutableSet<CloseContact>? = null,
 ) {
-
-    fun showBasicContactInfo() {
-        println("$photo \n$firstName ${secondName ?: ""} ${lastName ?: ""}")
-    }
-
-    fun showContactsPhoneNumbers() {
-        println("$mobileNumber \n$homeNumber")
-    }
-
-    fun showContactsICloud() {
-        println(iCloud)
-    }
-
-    fun showClosePeopleSet() {
-        closePeopleSet?.forEach { println(it) }
-    }
-}
-
-class ContactPage(user: Contact) {
-    val smsIcon = SquareIcon("иконка сообщения", "написать")
-    val callIcon = SquareIcon("иконка телефона", "вызов")
-    val videoCallIcon = SquareIcon("иконка камеры", "видео")
-    val emailIcon = SquareIcon("иконка конверта", "почта")
-
-    val faceTimeVideoCall = RoundIcon("иконка камеры")
-    val faceTimeCall = RoundIcon("иконка телефона")
-
-
-    fun showActionButtons () {
-        println("""
-            ${smsIcon.icon} | ${callIcon.icon} | ${videoCallIcon.icon} | ${emailIcon.icon}
-            ${smsIcon.iconName} | ${callIcon.iconName} | ${videoCallIcon.iconName} | ${emailIcon.iconName}
-        """.trimIndent())
-    }
-
-    fun showFaceTimeButtons() {
-        println("${faceTimeVideoCall.showIcon()} | ${faceTimeCall.showIcon()}")
-    }
-
-    fun showContactPage(user: Contact): String {
-        return (
-                """
-            ${user.showBasicContactInfo()}
-            ${showActionButtons()}
-            ${user.showContactsPhoneNumbers()}
-            ${showFaceTimeButtons()}
-            ${user.showContactsICloud()}
-            ${user.showClosePeopleSet()}
+    fun getContactInfo() {
+        return println(
+            """
+            $photo 
+            $firstName ${secondName ?: ""} ${lastName ?: ""}
+            $mobileNumber
+            $homeNumber
+            $iCloud 
         """.trimIndent()
-                )
+        )
     }
 
-    fun sendSms(mobileNumber: String) {
+    fun getCloseContact(): String {
+        return closePeopleSet?.forEach { println(it.name) }.toString()
+    }
+
+    fun sendSms() {
         val massage = readln()
         println("Отправь на номер $mobileNumber сообщение: \"$massage\"")
     }
 
-    fun startCall(mobileNumber: String) {
+    fun startCall() {
         val massage = readln()
         println("Отправь на номер $mobileNumber сообщение: \"$massage\"")
     }
 
-    fun startVideoCall(mobileNumber: String) {
+    fun startVideoCall() {
         println("Видеозвонок на номер $mobileNumber")
     }
 
-    fun startFaceTimeCall(mobileNumber: String) {
+    fun startFaceTimeCall() {
         val massage = readln()
         println("Звонок на номер $mobileNumber")
     }
 
-    fun startFaceTimeVideoCall(mobileNumber: String) {
+    fun startFaceTimeVideoCall() {
         println("Видеозвонок на номер $mobileNumber")
     }
 }
