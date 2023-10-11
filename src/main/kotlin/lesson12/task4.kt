af82739c10784083b3d1ca9d436f6cf2b7ba9e80
pugnitator <pugnitator2000@gmail.com>
2023-10-11 11:46:59 +0300

    Lesson 12. Fix the same classes names tasks3,4

--- src/main/kotlin/lesson12/task4.kt
+++ src/main/kotlin/lesson12/task4.kt
@@ -1,11 +1,11 @@
 package lesson12
 
 fun main() {
-    val weatherProgram1 = WeatherProgram2(45.4F, 10.4F, true, 740)
-    val weatherProgram2 = WeatherProgram2(25.7F, 19.4F, false, 755)
-    val weatherProgram3 = WeatherProgram2(23.7F, 11.4F, false, 758)
+    val weatherProgram1 = WeatherProgram3(45.4F, 10.4F, true, 740)
+    val weatherProgram2 = WeatherProgram3(25.7F, 19.4F, false, 755)
+    val weatherProgram3 = WeatherProgram3(23.7F, 11.4F, false, 758)
 }
-class WeatherProgram2(
+class WeatherProgram3(
     val daytimeTemperature: Float = 0.0F,
     val nightTemperature: Float = 0.0F,
     val isItRain: Boolean = false,
