b3422910ab02a2b4c5e1b8a831492da125cd3514
pugnitator <pugnitator2000@gmail.com>
2023-10-20 10:55:24 +0300

    U1-14-3. Fix by comments

--- src/main/kotlin/lesson14/task3.kt
+++ src/main/kotlin/lesson14/task3.kt
@@ -9,26 +9,26 @@ fun main() {
     val blackRectangle = Rectangle(BLACK_COLOR, 8.1f, 20.23f)
     val wightRectangle = Rectangle(WIGHT_COLOR, 7.77f, 10.54f)
 
-    val figureList : MutableList<Figure> = mutableListOf<Figure>(blackCircle, wightCircle, blackRectangle, wightRectangle)
-//    var sumBlackFiguresPerimeter = figureList.filter { it.color == BLACK_COLOR }.forEach{it.calculateThePerimeterOfTheFigure}
+    val figureList: MutableList<Figure> =
+        mutableListOf<Figure>(blackCircle, wightCircle, blackRectangle, wightRectangle)
 
-    var sumBlackFiguresPerimeter = figureList.filter { it.color == BLACK_COLOR }.map.it.calculateThePerimeterOfTheFigure.sum()
-    var sumWightFiguresArea = 0.00f
+    val sumBlackFiguresPerimeter =
+        figureList.filter { it.color == BLACK_COLOR }.map { it.calculateThePerimeterOfTheFigure() }.sum()
+    val sumWightFiguresArea =
+        figureList.filter { it.color == BLACK_COLOR }.map { it.calculateTheAreaOfTheFigure() }.sum()
 
-//    for (i: Int in 0 until figureList.size) {
-//        if (figureList[i].color == "чёрный") sumBlackFiguresPerimeter += figureList[i].calculateThePerimeterOfTheFigure()
-//        if (figureList[i].color == "белый") sumWightFiguresArea += figureList[i].calculateTheAreaOfTheFigure()
-//    }
 
-    println("""
-        Сумма периметров чёрных фигур: $sumBlackFiguresPerimeter
-        Сумма площадей белых фигур: $sumWightFiguresArea
-    """.trimIndent())
+    println(
+        """
+        Сумма периметров чёрных фигур: ${String.format("%.2f", sumBlackFiguresPerimeter)}
+        Сумма площадей белых фигур: ${String.format("%.2f", sumWightFiguresArea)}
+    """.trimIndent()
+    )
 }
 
 abstract class Figure(val color: String) {
-    open abstract fun calculateTheAreaOfTheFigure(): Float
-    open abstract fun calculateThePerimeterOfTheFigure(): Float
+    abstract fun calculateTheAreaOfTheFigure(): Float
+    abstract fun calculateThePerimeterOfTheFigure(): Float
 }
 
 class Circle(
@@ -38,6 +38,7 @@ class Circle(
     override fun calculateTheAreaOfTheFigure(): Float {
         return (PI * (radius.pow(2))).toFloat()
     }
+
     override fun calculateThePerimeterOfTheFigure(): Float {
         return (PI * 2 * radius).toFloat()
     }
@@ -51,6 +52,7 @@ class Rectangle(
     override fun calculateTheAreaOfTheFigure(): Float {
         return height * width
     }
+
     override fun calculateThePerimeterOfTheFigure(): Float {
         return (height + width) * 2
     }
