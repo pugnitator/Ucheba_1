20a0844c8b5d2b36d05d9a54cfa7b3ff01d6e757
pugnitator <pugnitator2000@gmail.com>
2023-11-06 16:50:58 +0300

    U1-15-2. local commit1

--- src/main/kotlin/lesson15/task2.kt
+++ src/main/kotlin/lesson15/task2.kt
@@ -2,40 +2,39 @@ package lesson15
 
 import java.util.*
 
+fun main() {
+    val temperature1 = Temperature(, Date(2023,11, 11), 10.12f )
+    val precipitationAmount = PrecipitationAmount ("Температура", Date(2023,11, 11), 10.12f )
+}
 
-class WeatherServer(
-    val listOfTemperature: MutableList<Temperature>,
-
-    ) {
-    fun sendTemperatureMassage(temperature: Temperature) {
-        return println("Дата: ${temperature.date}, температура: ${temperature.temperature}")
-    }
-
-    fun sendPrecipitationAmountMassage (precipitationAmount: PrecipitationAmount) {
-        return println("Дата: ${precipitationAmount.date}, температура: ${precipitationAmount.precipitationAmount}")
-    }
-
-    fun sendToTheServer (data: Any) {
-
-
-
-        if (data == Temperature) {
-
-        } else if (data == PrecipitationAmount) {
-
-        }else println("Ошибка отправки.")
+class WeatherServer() {
+    fun sendToTheServer(data: Any) {
+        if (data is Temperature) println(data.getInfo())
+        else if (data is PrecipitationAmount) println(data.getInfo())
+        else println("Ошибка отправки.")
     }
 }
 
-abstract class WeatherStationStats(val type: String)
+abstract class WeatherStationStats(val type: String) {
+    abstract fun getInfo()
+}
+
 class Temperature(
-    type: String = "Температура",
+    type: String,
     val date: Date,
     val temperature: Float,
-) : WeatherStationStats(type)
+) : WeatherStationStats(type = "Температура") {
+    override fun getInfo() {
+        return println("Дата: $date, температура: $temperature")
+    }
+}
 
 class PrecipitationAmount(
-    type: String = "Количество осадков",
-    val date : Date,
+    type: String,
+    val date: Date,
     val precipitationAmount: Float,
-) : WeatherStationStats (type)
+) : WeatherStationStats(type = "Температура") {
+    override fun getInfo() {
+        return println("Дата: $date, температура: $precipitationAmount")
+    }
+}
