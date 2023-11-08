d398e092e1f7717d65b92c5e78575b8e123abab8
pugnitator <pugnitator2000@gmail.com>
2023-11-08 11:20:01 +0300

    U1-15-3. User roles. Available actions

---
+++ src/main/kotlin/lesson15/task3.kt
@@ -0,0 +1,35 @@
+package lesson15
+
+fun main() {
+
+    val admin = AdminUser(1, "admin")
+    val user = SimpleUser(2, "Бритни")
+
+    admin.sendMassage()
+    admin.readMassage()
+    admin.deleteMassage()
+    println()
+    user.sendMassage()
+    user.readMassage()
+
+}
+
+abstract class User15(val id: Int, val name: String) : AllUserAction
+
+class AdminUser(id: Int, name: String) : User15(id, name) {
+    fun deleteMassage() {
+        println("Жмём кнопку для удаления сообщения")
+    }
+}
+
+class SimpleUser(id: Int, name: String) : User15(id, name)
+
+interface AllUserAction {
+    fun readMassage() {
+        println("Жмём кнопку для прочтения сообщения")
+    }
+
+    fun sendMassage() {
+        println("Жмём кнопку для отправки сообщения")
+    }
+}
