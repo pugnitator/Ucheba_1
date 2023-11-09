361777ad81be39194171a5ea900688cd26c90e93
pugnitator <pugnitator2000@gmail.com>
2023-11-09 22:19:53 +0300

    U1-15-4. Local commit

--- src/main/kotlin/lesson15/task4.kt
+++ src/main/kotlin/lesson15/task4.kt
@@ -2,6 +2,11 @@ package lesson15
 
 fun main() {
 
+    val harmonica = MusicalInstrument("harm", 12)
+    val harmonicaCase = Component("футляр", 90)
+
+    harmonica.findProduct(harmonicaCase.name)
+
 }
 
 abstract class Good(
@@ -12,21 +17,15 @@ abstract class Good(
 class MusicalInstrument(
     name: String,
     quantityInStock: Int,
-    val listOfComponents: MutableList <Component>
-) : Good(name, quantityInStock) {
-
-}
+) : Good(name, quantityInStock), ProductSearch
 
 class Component(
     name: String,
     quantityInStock: Int,
-    val musicalInstrument: MusicalInstrument,
 ) : Good(name, quantityInStock)
 
 interface ProductSearch {
-
-    fun findProduct (searchQuery: String) {
-        println("Поиск товара $searchQuery")
+    fun findProduct(searchQuery: String) {
+        println("Поиск товара \"$searchQuery\"")
     }
-
 }
