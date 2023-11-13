f12a2a84fbdd9c2c5310d953c1e7c7462b02d56f
pugnitator <pugnitator2000@gmail.com>
2023-11-13 11:28:38 +0300

    U1-15-5. Local commit 2. Interfaces for passenger car and truck

--- src/main/kotlin/lesson15/task5.kt
+++ src/main/kotlin/lesson15/task5.kt
@@ -2,32 +2,44 @@ package lesson15
 
 fun main() {
 
-    val passengerCar = PassengerCar("volvo", 3, )
+    val passengerCar = PassengerCar("volvo", 3, 0)
+    val numberOfPeopleToTransport = 6
+
+
+    println()
+
+//    val truck = Truck("Камаз", 1, 1, 1, 2.0f)
+//    truck.loadPassengers(1)
+//    truck.startDrive()
+//    truck.stopDrive()
+//    truck.loadCargo(2.0f)
+//    truck.startDrive()
+//    truck.transportCargo("Внуково", "Калуга")
+//    truck.stopDrive()
+//    truck.unloadCargo(2.0f)
 
 }
 
 abstract class Car(
     val model: String,
     val maxNumberOfPassengers: Int,
-    val minNumberOfPassengers: Int,
-    var currentNumberOfPassengers: Int = 0,
+    var currentNumberOfPassengers: Int,
 ) : FunctionsForAllCar
-
-class Truck(
-    model: String,
-    maxNumberOfPassengers: Int,
-    minNumberOfPassengers: Int = 1,
-    currentNumberOfPassengers: Int,
-) : Car(model, maxNumberOfPassengers, minNumberOfPassengers = 1, currentNumberOfPassengers = 0,)
 
 class PassengerCar(
     model: String,
     maxNumberOfPassengers: Int,
-    minNumberOfPassengers: Int,
+    currentNumberOfPassengers: Int,
+) : Car(model, maxNumberOfPassengers, currentNumberOfPassengers)
+
+
+class Truck(
+    model: String,
+    maxNumberOfPassengers: Int,
     currentNumberOfPassengers: Int,
     val maxLoadCapacity: Float,
     var currentCargoAmount: Float = 0.00f,
-) : Car(model, maxNumberOfPassengers, minNumberOfPassengers = 1, currentNumberOfPassengers = 0,)
+) : Car(model, maxNumberOfPassengers, currentNumberOfPassengers), ActionsWithCargo
 
 interface FunctionsForAllCar {
     fun startDrive() {
@@ -38,29 +50,45 @@ interface FunctionsForAllCar {
         println("Прекратить движение.")
     }
 
-    fun loadPassengers(numberOfPeople: Int) {
-        println("В автомобиль сели $numberOfPeople человек(а)")
+    fun loadPassengers(numberOfPeople: Int, maxNumberOfPeople: Int): Int {
+        var numberOfLoadPeople = 0
+        if (numberOfPeople > maxNumberOfPeople) numberOfLoadPeople = maxNumberOfPeople
+        else numberOfLoadPeople = numberOfPeople
+        println("Произведена посадка $numberOfLoadPeople пассажира(ов)")
+        return numberOfLoadPeople
     }
 
     fun unloadPassengers(numberOfPeople: Int) {
         println("Из автомобиля вышли $numberOfPeople человек(а)")
     }
 
-    fun transportPassengers(pointA: String, pointB: String) {
-        println("Забрать пассажиров по адресу $pointA и привезти по адресу $pointB")
-    }
-}
+    fun transportPassengers(car: Any, numberOfPeopleToTransport: Int, maxLoadCapacity: Int) {
+        if (car is Truck || car is PassengerCar) {
+            var deliveredPeople = 0
+            do {
+                deliveredPeople += passengerCar.loadPassengers(
+                    numberOfPeopleToTransport,
+                    passengerCar.maxNumberOfPassengers
+                )
+                passengerCar.transportPassengers("Москва", "Петушки")
+                passengerCar.startDrive()
+                passengerCar.stopDrive()
+                passengerCar.unloadPassengers(numberOfPeopleToTransport)
+            } while (deliveredPeople != numberOfPeopleToTransport)
 
-interface ActionsWithCargo {
-    fun loadCargo(cargoWeight: Float) {
-        println("Загрузить груз весом $cargoWeight")
+        } else println("Перевозка невозможна")
     }
 
-    fun unloadCargo(cargoWeight: Float) {
-        println("Выгрузить груз весом $cargoWeight")
-    }
+    interface ActionsWithCargo {
+        fun loadCargo(cargoWeight: Float) {
+            println("Загрузить груз весом $cargoWeight")
+        }
 
-    fun transportCargo(pointA: String, pointB: String) {
-        println("Забрать груз по адресу $pointA и привезти по адресу $pointB")
+        fun unloadCargo(cargoWeight: Float) {
+            println("Выгрузить груз весом $cargoWeight")
+        }
+
+        fun transportCargo(pointA: String, pointB: String) {
+            println("Забрать груз по адресу $pointA и привезти по адресу $pointB")
+        }
     }
-}
