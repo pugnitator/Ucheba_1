0706e6f382d24f0e367bc2932412d8ad344b9a76
pugnitator <pugnitator2000@gmail.com>
2023-11-16 19:51:41 +0300

    U1-16-5. Game. Hero vs Villain. Fix by task

--- src/main/kotlin/lesson16/task5.kt
+++ src/main/kotlin/lesson16/task5.kt
@@ -7,38 +7,47 @@ fun main() {
     player1.takeDamage(player2.getAttackDamage())
     player1.heal(5)
     player1.takeDamage(100)
+    player1.heal(100)
 }
 
 class Player(
     val name: String,
     private var health: Int = HEALTH,
-    private val attackDamage: Int = ATTACK_DAMAGE,
+    private var attackDamage: Int = ATTACK_DAMAGE,
 ) {
     fun getHealth() = health
+
     fun getAttackDamage() = attackDamage
+
     fun takeDamage(damage: Int) {
         val currentHealth = health - damage
-        if (currentHealth <= HEALTH_TO_DIE) {
-            println("Урон от удара: $damage. Game over.")
-
-            for (i in (1..SECONDS_TO_REVIVAL).reversed()) {
-                println("До возрождения $i сек.")
-                Thread.sleep(1000)
-            }
-
-            health = HEALTH
-            println("Возрождение на базе. Здоровье: $health")
-        } else {
+        if (currentHealth <= HEALTH_TO_DIE) println(dai())
+        else {
             health = currentHealth
             println("Урон от удара: $damage, текущее здоровье: $health")
         }
     }
 
     fun heal(amountOfHeal: Int) {
-        val currentHealth = health + amountOfHeal
-        health = if (currentHealth > HEALTH) HEALTH
-        else currentHealth
-        println("Здоровье восстановлено на $amountOfHeal, текущее здоровье: $health")
+        when (health) {
+            HEALTH_TO_DIE -> {
+                println("Вы мертвы, лечение вас не спасёт.")
+                return
+            }
+
+            else -> {
+                val currentHealth = health + amountOfHeal
+                health = if (currentHealth > HEALTH) HEALTH
+                else currentHealth
+                println("Здоровье восстановлено на $amountOfHeal, текущее здоровье: $health")
+            }
+        }
+    }
+
+    private fun dai(): String {
+        health = VALUE_FOR_THE_DEATH
+        attackDamage = VALUE_FOR_THE_DEATH
+        return "Game over. Здоровье: $health, сила удара: $attackDamage."
     }
 }
 
@@ -46,4 +55,4 @@ class Player(
 const val HEALTH = 100
 const val HEALTH_TO_DIE = 0
 const val ATTACK_DAMAGE = 10
-const val SECONDS_TO_REVIVAL = 5
+const val VALUE_FOR_THE_DEATH = 0
