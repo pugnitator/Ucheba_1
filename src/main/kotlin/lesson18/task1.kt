ea5f5774e5175e2b17956da3fbfb86b548bda6c6
pugnitator <pugnitator2000@gmail.com>
2023-11-21 11:34:27 +0300

    U1-18-1. Local commit

--- src/main/kotlin/lesson18/task1.kt
+++ src/main/kotlin/lesson18/task1.kt
@@ -1,18 +1,23 @@
 package lesson18
 
 fun main() {
-    val order1 = OrderForOneProduct(123, "Мороженое")
-    val order2 = OrderForSeveralProducts(854, arrayOf("Курочка", "Банан", "Чай"))
+    val order1 = OnlineOrder(123, "Мороженое")
+    val order2 = OnlineOrder(854, arrayOf("Курочка", "Банан", "Чай"))
 
-    val listOfOrders = mutableListOf<OnlineOrder>(order1, order2)
-    listOfOrders.forEach { it.getOnlineOrderInfo() }
+//    val listOfOrders = mutableListOf<OnlineOrder>(order1, order2)
+//    listOfOrders.forEach { it.getOnlineOrderInfo() }
 
 }
 
-abstract class OnlineOrder(
+class OnlineOrder(
     private val orderNumber: Int,
+    private val product: Any,
 ) {
-    abstract fun getOnlineOrderInfo()
+    fun getOnlineOrderInfo() {
+
+
+    }
+//    } = println("Заказан товар: $product.")
 }
 
 class OrderForOneProduct(
