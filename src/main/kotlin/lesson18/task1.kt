c74633125e5b8e5d6fc07dff08a12dd87e039ce7
pugnitator <pugnitator2000@gmail.com>
2023-11-28 10:13:36 +0300

    U1-18-1. Code refactoring. Ad hoc

--- src/main/kotlin/lesson18/task1.kt
+++ src/main/kotlin/lesson18/task1.kt
@@ -1,37 +1,16 @@
 package lesson18
 
 fun main() {
-    val order1 = OnlineOrder(123, "Мороженое")
-    val order2 = OnlineOrder(854, arrayOf("Курочка", "Банан", "Чай"))
+    val order1 = OnlineOrder("123")
+    val order2 = OnlineOrder("854")
 
-//    val listOfOrders = mutableListOf<OnlineOrder>(order1, order2)
-//    listOfOrders.forEach { it.getOnlineOrderInfo() }
-
+    order1.getOneProductOrderInfo("Молоко")
+    order2.getOneProductOrderInfo(arrayOf("Курочка", "Банан", "Чай"))
 }
 
-class OnlineOrder(
-    private val orderNumber: Int,
-    private val product: Any,
-) {
-    fun getOnlineOrderInfo() {
-
-
-    }
-//    } = println("Заказан товар: $product.")
-}
-
-class OrderForOneProduct(
-    orderNumber: Int,
-    private val product: String,
-) : OnlineOrder(orderNumber) {
-    override fun getOnlineOrderInfo() = println("Заказан товар: $product.")
-}
-
-class OrderForSeveralProducts(
-    orderNumber: Int,
-    private val products: Array<String>,
-) : OnlineOrder(orderNumber) {
-    override fun getOnlineOrderInfo() {
+class OnlineOrder(private val orderNumber: String) {
+    fun getOneProductOrderInfo(product: String) = println("Заказан товар: $product.")
+    fun getOneProductOrderInfo(products: Array<String>) {
         println("Заказаны следующие товары:")
         for (i in products) println(i)
     }
