50a47883920e1e0ebc32b91996129ce950e22bd6
pugnitator <pugnitator2000@gmail.com>
2023-11-29 10:46:12 +0300

    U1-18-4. Code refactoring

--- src/main/kotlin/lesson18/task4.kt
+++ src/main/kotlin/lesson18/task4.kt
@@ -3,20 +3,21 @@ package lesson18
 import kotlin.math.pow
 
 fun main() {
-    val cubePackage = Package()
-    val rectanglePackage = Package()
+    val cubePackage = CubePackage(12.5f)
+    val rectanglePackage = RectanglePackage(6.4f, 4.7f, 9.1f)
 
-    println(cubePackage.getPackageSurfaceArea(12.5f))
-    println(rectanglePackage.getPackageSurfaceArea(6.4f, 4.7f, 9.1f))
+    println(cubePackage.getPackageSurfaceArea())
+    println(rectanglePackage.getPackageSurfaceArea())
 }
 
-class Package {
-    fun getPackageSurfaceArea(
-        length: Float,
-        width: Float,
-        high: Float,
-    ) = 2 * ((length * width) + (length * high) + (width * high))
-
-    fun getPackageSurfaceArea(cubeSideLength: Float) = 6 * cubeSideLength.pow(2)
+abstract class Package() {
+    abstract fun getPackageSurfaceArea(): Float
 }
 
+class CubePackage(private val cubeSideLength: Float) : Package() {
+    override fun getPackageSurfaceArea() = 6 * cubeSideLength.pow(2)
+}
+
+class RectanglePackage(private val length: Float, private val width: Float, private val high: Float) : Package() {
+    override fun getPackageSurfaceArea() = 2 * ((length * width) + (length * high) + (width * high))
+}
