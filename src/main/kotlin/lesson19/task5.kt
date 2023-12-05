c39346b3e8946269436ddfabc11dc6be7bd0b3e9
pugnitator <pugnitator2000@gmail.com>
2023-12-05 18:02:21 +0300

    U1-19-5. Card index. Enum

--- src/main/kotlin/lesson19/task5.kt
+++ src/main/kotlin/lesson19/task5.kt
@@ -3,42 +3,49 @@ package lesson19
 fun main() {
     val listOnPersons = mutableListOf<Person>()
 
-    for (i in 0 until 5) {
+    for (i in 0 until NUMBER_OF_PEOPLE_TO_PRINT) {
         print("Заполните данные о себе. \nВведите имя: ")
-        val name = readln()
+        val person = Person(readln())
 
         println("Укажите пол, выбрав один из вариантов:")
         for (i in Sex.values()) println(i.getSexName())
+        person.setSex()
 
-        var sex = Sex.MALE
-        do {
-            when (readln().lowercase()) {
-                Sex.MALE.getSexName().lowercase() -> sex = Sex.MALE
-                Sex.FEMALE.getSexName().lowercase() -> sex = Sex.FEMALE
-                else -> println("Повторите попытку.")
-            }
-        } while ((sex != Sex.MALE) && (sex != Sex.FEMALE))
-
-        val person = Person(name, sex)
-        println("Имя: ${person.getName()}, пол: ${person.getSex()}")
         listOnPersons.add(person)
     }
 
     listOnPersons.forEach { println("Имя: ${it.getName()}, пол: ${it.getSex()} ") }
-
 }
 
 class Person(
     private val name: String,
-    private var sex: Sex,
+    private var sex: Sex = Sex.IS_EMPTY,
 ) {
+    fun setSex() {
+        var inputSex: Sex
+        do {
+            inputSex = when (readln().lowercase()) {
+                Sex.MALE.getSexName().lowercase() -> Sex.MALE
+                Sex.FEMALE.getSexName().lowercase() -> Sex.FEMALE
+                else -> {
+                    println("Повторите попытку.")
+                    Sex.IS_EMPTY
+                }
+            }
+        } while (inputSex == Sex.IS_EMPTY)
+        sex = inputSex
+    }
+
     fun getName() = name
     fun getSex() = sex.getSexName()
 }
 
 enum class Sex(private val sexName: String) {
+    IS_EMPTY("Не указано"),
     MALE("Мужчина"),
     FEMALE("Женщина");
 
     fun getSexName() = sexName
 }
+
+const val NUMBER_OF_PEOPLE_TO_PRINT = 5
