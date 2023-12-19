b4c7d7945d2db8377b6660d6cb7b89c4b0f2150f
pugnitator <pugnitator2000@gmail.com>
2023-12-19 21:16:40 +0300

    U1-20-5. local commit 2

--- src/main/kotlin/lesson20/task5.kt
+++ src/main/kotlin/lesson20/task5.kt
@@ -1,29 +1,26 @@
 package lesson20
 
-class Robot(
-    val listOfModifiers: MutableList<Unit> = mutableListOf(
-        {phrase: String -> println(phrase.reversed())}(a),
+fun main() {
+    val robot = Robot()
+    robot.setModifier ({ it: String -> String
+        it.reversed()
+    }())
+}
 
-    ),
-    val listOfPhrases: MutableList<String> = mutableListOf<String>(
+
+
+class Robot(
+    val listOfModifiers: MutableList<(Unit)> = mutableListOf(),
+    val listOfPhrases: MutableList<String> = mutableListOf(
         "Одна ошибка - и ты ошибся",
         "Утро вечера мудренее",
         "Действуй, сестра",
         "Мне нужна твоя одежда, ботинки и мотоцикл",
         "Убить всех человеков"
-    )
-
+    ),
 ) {
 
-    fun say() {
-
-        listOfPhrases.random()
-
-    }
-
-    fun setModifier(modifier: Unit) : String {
-
-    }
-
+    fun say() = println(listOfModifiers.last())
+    fun setModifier(modifier: String) {listOfModifiers.add(modifier)}
 
 }
