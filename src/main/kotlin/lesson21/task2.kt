61946fd751bb8f6d828246b2976cd034a8ccb233
pugnitator <pugnitator2000@gmail.com>
2024-01-04 18:05:50 +0300

    U1-21-2. Code refactoring

--- src/main/kotlin/lesson21/task2.kt
+++ src/main/kotlin/lesson21/task2.kt
@@ -1,16 +1,8 @@
 package lesson21
 
 fun main() {
-    val listOfNumbers = mutableListOf<Int>(1, 2, 3, 4, 5, 2)
+    val listOfNumbers = mutableListOf<Int>(1, 2, 3, 4, 5, 2, 6)
     println(listOfNumbers.evenNumbersSum())
 }
 
-fun MutableList<Int>.evenNumbersSum(): Int {
-    var evenNumberSum = 0
-    for (i in asSequence()) {
-        when (i % 2) {
-            0 -> evenNumberSum += i
-        }
-    }
-    return evenNumberSum
-}
+fun MutableList<Int>.evenNumbersSum() = asSequence().filter { it % 2 == 0 }.sum()
